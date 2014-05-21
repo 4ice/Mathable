9b85b0f01a9a9301a0623551ce6407eb5807e215
David Andersson <da.ch.andersson@gmail.com>
2014-05-21 23:33:54 +0200

    Todays work

--- Mathable/src/se/DV1456/mathable/MainActivity.java
+++ Mathable/src/se/DV1456/mathable/MainActivity.java
@@ -1,29 +1,23 @@
 package se.DV1456.mathable;
 
-import org.andengine.engine.camera.BoundCamera;
-import org.andengine.engine.camera.Camera;
+import org.andengine.engine.camera.ZoomCamera;
 import org.andengine.engine.handler.IUpdateHandler;
-import org.andengine.engine.options.EngineOptions;
-import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.*;
 import org.andengine.engine.options.resolutionpolicy.FillResolutionPolicy;
-import org.andengine.entity.Entity;
 import org.andengine.entity.primitive.Rectangle;
-import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.*;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.entity.util.FPSLogger;
-import org.andengine.extension.tmx.TMXLayer;
-import org.andengine.extension.tmx.TMXLoader;
-import org.andengine.extension.tmx.TMXProperties;
-import org.andengine.extension.tmx.TMXTile;
-import org.andengine.extension.tmx.TMXTileProperty;
-import org.andengine.extension.tmx.TMXTiledMap;
+import org.andengine.entity.util.*;
+import org.andengine.extension.tmx.*;
 import org.andengine.extension.tmx.TMXLoader.ITMXTilePropertiesListener;
 import org.andengine.extension.tmx.util.exception.TMXLoadException;
 import org.andengine.input.touch.TouchEvent;
+import org.andengine.input.touch.detector.*;
+import org.andengine.input.touch.detector.PinchZoomDetector.IPinchZoomDetectorListener;
+import org.andengine.input.touch.detector.ScrollDetector.IScrollDetectorListener;
 import org.andengine.opengl.texture.TextureOptions;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
-import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.*;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.SimpleBaseGameActivity;
 import org.andengine.util.Constants;
@@ -39,7 +33,7 @@ import android.widget.Toast;
  * @author Nicolas Gramlich
  * @since 15:13:46 - 15.06.2010
  */
-public class MainActivity extends SimpleBaseGameActivity {
+public class MainActivity extends SimpleBaseGameActivity implements IOnSceneTouchListener, IScrollDetectorListener, IPinchZoomDetectorListener {
 	// ===========================================================
 	// Constants
 	// ===========================================================
@@ -50,11 +44,23 @@ public class MainActivity extends SimpleBaseGameActivity {
 	// ===========================================================
 	// Fields
 	// ===========================================================
-
+	
+	//Screen resolution
 	private int CAMERA_WIDTH;
 	private int CAMERA_HEIGHT;
+	
+	private ZoomCamera mZoomCamera;
+	
+	//Equal to jFrame in std java.
+	private Scene mScene;
+	
+	private SurfaceScrollDetector mScrollDetector;
+	private PinchZoomDetector mPinchZoomDetector;
+	private float mPinchZoomStartedCameraZoomFactor;
+	
 	private BitmapTextureAtlas mBitmapTextureAtlas;
 	private ITextureRegion mFaceTextureRegion;
+	
 	private TMXTiledMap mTMXTiledMap;
 	protected int nrOfBlanks;
 
@@ -73,14 +79,16 @@ public class MainActivity extends SimpleBaseGameActivity {
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		
-		//Gets the screen resolution and saves it in CAMERA_WIDTH and HEIGHT.
+		//Gets the screen resolution and saves it in CAMERA_WIDTH and CAMERA_HEIGHT.
 		this.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 		this.CAMERA_WIDTH = displayMetrics.widthPixels;
 		this.CAMERA_HEIGHT = displayMetrics.heightPixels;
 		
-		Camera camera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
 		
-		return new EngineOptions(true, ScreenOrientation.PORTRAIT_FIXED, new FillResolutionPolicy(), camera);
+		//Init the camera
+		this.mZoomCamera = new ZoomCamera(0, 0, this.CAMERA_WIDTH, this.CAMERA_HEIGHT);
+		
+		return new EngineOptions(true, ScreenOrientation.PORTRAIT_SENSOR, new FillResolutionPolicy(), this.mZoomCamera);
 		
 	}
 
@@ -100,10 +108,49 @@ public class MainActivity extends SimpleBaseGameActivity {
 	public Scene onCreateScene() {
 		this.mEngine.registerUpdateHandler(new FPSLogger());
 		
-		final Scene scene = new Scene();
+		this.mScene = new Scene();
+		this.mScene.setOnAreaTouchTraversalFrontToBack();
 		
+		//scroll is for moving around the board
+		this.mScrollDetector = new SurfaceScrollDetector(this);
+		//pinch is for zooming
+		this.mPinchZoomDetector = new PinchZoomDetector(this);
+
+		//listener
+		this.mScene.setOnSceneTouchListener(this);
+		//enable touch events in the scene
+		this.mScene.setTouchAreaBindingOnActionDownEnabled(true);
 		
+		//1. Load TMX
+		try {
+			final TMXLoader tmxLoader = new TMXLoader(this.getAssets(), this.mEngine.getTextureManager(), TextureOptions.BILINEAR_PREMULTIPLYALPHA, this.getVertexBufferObjectManager(), new ITMXTilePropertiesListener() {
+				@Override
+				public void onTMXTileWithPropertiesCreated(final TMXTiledMap pTMXTiledMap, final TMXLayer pTMXLayer, final TMXTile pTMXTile, final TMXProperties<TMXTileProperty> pTMXTileProperties) {
+					/* We are going to count the tiles that have the property "cactus=true" set. */
+					if(pTMXTileProperties.containsTMXProperty("property", "0")) {
+						MainActivity.this.nrOfBlanks++;
+						}
+					}
+				});
+			this.mTMXTiledMap = tmxLoader.loadFromAsset("tmx/gameBoard.tmx");
+
+			this.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Toast.makeText(MainActivity.this, "There are: " + MainActivity.this.nrOfBlanks + " Blank tiles left...", Toast.LENGTH_LONG).show();
+					}
+				});
+			} catch (final TMXLoadException e) {
+				Debug.e(e);
+			}
 		
+		//Config the camera
+		final TMXLayer tmxLayer = this.mTMXTiledMap.getTMXLayers().get(0);
+		
+
+		this.mScene.attachChild(tmxLayer);
+		
+		//Create a tile on the screen
 		final float centerX = (this.CAMERA_WIDTH - this.mFaceTextureRegion.getWidth()) / 2;
 		final float centerY = (this.CAMERA_HEIGHT - this.mFaceTextureRegion.getHeight()) / 2;
 		final Sprite face = new Sprite(centerX, centerY, this.mFaceTextureRegion, this.getVertexBufferObjectManager()) {
@@ -117,39 +164,17 @@ public class MainActivity extends SimpleBaseGameActivity {
 				return true;
 			}
 		};
+		this.mScene.attachChild(face);
 		
-		//1. Create a rectangle with the grid that has the gameboard
-		try {
-			final TMXLoader tmxLoader = new TMXLoader(this.getAssets(), this.mEngine.getTextureManager(), TextureOptions.BILINEAR_PREMULTIPLYALPHA, this.getVertexBufferObjectManager(), new ITMXTilePropertiesListener() {
-				@Override
-				public void onTMXTileWithPropertiesCreated(final TMXTiledMap pTMXTiledMap, final TMXLayer pTMXLayer, final TMXTile pTMXTile, final TMXProperties<TMXTileProperty> pTMXTileProperties) {
-					/* We are going to count the tiles that have the property "cactus=true" set. */
-					if(pTMXTileProperties.containsTMXProperty("property", "0")) {
-						MainActivity.this.nrOfBlanks++;
-					}
-				}
-			});
-			this.mTMXTiledMap = tmxLoader.loadFromAsset("tmx/gameBoard.tmx");
-
-			this.runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					Toast.makeText(MainActivity.this, "There are: " + MainActivity.this.nrOfBlanks + " Blank tiles left...", Toast.LENGTH_LONG).show();
-				}
-			});
-		} catch (final TMXLoadException e) {
-			Debug.e(e);
-		}
 		
-		final TMXLayer tmxLayer = this.mTMXTiledMap.getTMXLayers().get(0);
-		scene.attachChild(tmxLayer);
+		
 		
 		/* Now we are going to create a rectangle that will  always highlight the tile below the feet of the pEntity. */
 		final Rectangle currentTileRectangle = new Rectangle(0, 0, this.mTMXTiledMap.getTileWidth(), this.mTMXTiledMap.getTileHeight(), this.getVertexBufferObjectManager());
 		currentTileRectangle.setColor(1, 0, 0, 0.25f);
-		scene.attachChild(currentTileRectangle);
+		this.mScene.attachChild(currentTileRectangle);
 
-		scene.registerUpdateHandler(new IUpdateHandler() {
+		this.mScene.registerUpdateHandler(new IUpdateHandler() {
 			@Override
 			public void reset() { }
 
@@ -166,29 +191,84 @@ public class MainActivity extends SimpleBaseGameActivity {
 				}
 			}
 		});
-		
-		
-		
-		
-		
-		
-		
-		
+				
 		//2. create a row of 7 tiles that the user can put down on the table.
 		
-		scene.setBackground(new Background(0, 0, 0));
-		scene.attachChild(face);
-		scene.registerTouchArea(face);
-		scene.setTouchAreaBindingOnActionDownEnabled(true);
+		this.mScene.setBackground(new Background(0, 0, 0));
+		this.mScene.registerTouchArea(face);
+		this.mScene.setTouchAreaBindingOnActionDownEnabled(true);
 
-		return scene;
+		return this.mScene;
+	}
+
+	@Override
+	public void onScrollStarted(final ScrollDetector pScollDetector, final int pPointerID, final float pDistanceX, final float pDistanceY) {
+		this.zoomHelper(pDistanceX, pDistanceY);
+	}
+
+	@Override
+	public void onScroll(final ScrollDetector pScollDetector, final int pPointerID, final float pDistanceX, final float pDistanceY) {
+		this.zoomHelper(pDistanceX, pDistanceY);
+	}
+	
+	@Override
+	public void onScrollFinished(final ScrollDetector pScollDetector, final int pPointerID, final float pDistanceX, final float pDistanceY) {
+		this.zoomHelper(pDistanceX, pDistanceY);
+	}
+
+	@Override
+	public void onPinchZoomStarted(final PinchZoomDetector pPinchZoomDetector, final TouchEvent pTouchEvent) {
+		this.mPinchZoomStartedCameraZoomFactor = this.mZoomCamera.getZoomFactor();
+	}
+
+	@Override
+	public void onPinchZoom(final PinchZoomDetector pPinchZoomDetector, final TouchEvent pTouchEvent, final float pZoomFactor) {
+		this.mZoomCamera.setZoomFactor(this.mPinchZoomStartedCameraZoomFactor * pZoomFactor);
+	}
+
+	@Override
+	public void onPinchZoomFinished(final PinchZoomDetector pPinchZoomDetector, final TouchEvent pTouchEvent, final float pZoomFactor) {
+		this.mZoomCamera.setZoomFactor(this.mPinchZoomStartedCameraZoomFactor * pZoomFactor);
+	}
+
+
+	@Override
+	public boolean onSceneTouchEvent(final Scene pScene, final TouchEvent pSceneTouchEvent) {
+		this.mPinchZoomDetector.onTouchEvent(pSceneTouchEvent);
+
+		if(this.mPinchZoomDetector.isZooming()) {
+			this.mScrollDetector.setEnabled(false);
+		} else {
+			if(pSceneTouchEvent.isActionDown()) {
+				this.mScrollDetector.setEnabled(true);
+			}
+			this.mScrollDetector.onTouchEvent(pSceneTouchEvent);
+		}
+
+		return true;
 	}
 
 	// ===========================================================
 	// Methods
 	// ===========================================================
 
+	public void zoomHelper(final float pDistanceX, final float pDistanceY)
+	{
+		final float zoomFactor = this.mZoomCamera.getZoomFactor();
+		this.mZoomCamera.offsetCenter(-pDistanceX / zoomFactor, -pDistanceY / zoomFactor);
+	}
+	
 	// ===========================================================
 	// Inner and Anonymous Classes
 	// ===========================================================
 }
+
+
+/** USE FOR TEST OUTPUT! **/
+
+//this.runOnUiThread(new Runnable() {
+//	@Override
+//	public void run() {
+//		Toast.makeText(MainActivity.this, "test", Toast.LENGTH_LONG).show();
+//		}
+//});
